5ab3917851a3d37628a6a9e73259b4dc49e6c604
Bimsara <ravindu.bimsara97@gmail.com>
2023-11-08 19:15:38 +0530

    news app

---
+++ lib/screens/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("News"),
+            Text(
+              "App",
+              style: TextStyle(color: Colors.lightBlue.shade300),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Container(),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final image, categoryName;
+  CategoryTile({this.image, this.categoryName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
